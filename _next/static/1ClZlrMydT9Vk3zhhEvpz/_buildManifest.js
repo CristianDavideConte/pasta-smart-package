5f58af204b222f76f85dd0e1a85181388087e0ba
CristianDavideConte <64142998+CristianDavideConte@users.noreply.github.com>
2022-11-15 21:15:35 +0000

    Deploying to gh-pages from @ CristianDavideConte/pasta-smart-package@282aad588ecad30532bbe4e396637437c12c7d96 🚀

---
+++ _next/static/1ClZlrMydT9Vk3zhhEvpz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7a1c5e47e6078123.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
