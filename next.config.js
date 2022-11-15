bc05542875bb6df5fac96110147588dabc9443c9
Cristian Davide Conte <64142998+CristianDavideConte@users.noreply.github.com>
2022-11-15 19:55:48 +0100

    test

--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: "/pasta-smart-package", //Uncomment when pushing to github 
+  assetPrefix: "/pasta-smart-package" //Uncomment when pushing to github
+  /*
   images: {
     loader: "akamai",
     path: ""
   },
-  basePath: "/pasta-smart-package",
-  assetPrefix: "/pasta-smart-package"
+  */
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,7 @@ export default function Home() {
       <Image 
         src = {testImage} 
         alt = "picture of pasta"
+        width={100} height={100}
       />
     </>
   )
