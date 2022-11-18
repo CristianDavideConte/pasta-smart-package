d670a50349833f9ec9d03fa061e36147b0b4f71b
Cristian Davide Conte <64142998+CristianDavideConte@users.noreply.github.com>
2022-11-18 21:15:45 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,14 +3,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  /**
+  /**/
   images: {
     loader: "akamai",
     path: "",
   },
   basePath: "/pasta-smart-package", //Uncomment when pushing to github 
   assetPrefix: "/pasta-smart-package", //Uncomment when pushing to 
-  */
 }
 
 module.exports = nextConfig;
