0af506e5d25de5d0a2972df10541bc96fb256005
Cristian Davide Conte <64142998+CristianDavideConte@users.noreply.github.com>
2022-12-04 21:58:00 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  //basePath: "/pasta-smart-package", //Uncomment when pushing to github 
-  //assetPrefix: "/pasta-smart-package", //Uncomment when pushing to github
+  basePath: "/pasta-smart-package", //Uncomment when pushing to github 
+  assetPrefix: "/pasta-smart-package", //Uncomment when pushing to github
 }
 
 module.exports = nextConfig;
